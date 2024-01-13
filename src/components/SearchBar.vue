59c1898ecc348a831a0d798a984d75012f684f72
Huang Chuan <river1440>
2024-01-13 20:06:54 +0530

    chore: change bg color of search bar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -14,13 +14,13 @@ const query = defineModel("query", { type: String, default: "" });
 
 <template>
 	<div
-		class="border-gray-400 bg-white hover:border-cyan-500 hover:bg-cyan-50 group flex items-center justify-between self-stretch rounded-xl border border-solid p-4"
+		class="border-gray-400 bg-white hover:border-gray-500 hover:bg-gray-50 group flex items-center justify-between self-stretch rounded-xl border border-solid p-4"
 	>
 		<input
 			v-model="query"
 			type="text"
 			:placeholder="props.placeholder"
-			class="text-cyan-900 group-hover:bg-cyan-50 w-full text-base font-bold outline-none"
+			class="text-cyan-900 group-hover:bg-gray-50 w-full text-base font-bold outline-none"
 		/>
 		<AppIcon icon-name="MagnifyingGlassIcon" class="h-6 w-6" />
 	</div>
